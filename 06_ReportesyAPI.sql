956d4b588daeb6bd8b8e8917bed65b37bd525528
Patricio Rodriguez <159083683+PatricioRodriguez080@users.noreply.github.com>
2025-11-02 20:51:48 -0300

    Rename 05_ReportesyAPI.sql to 06_ReportesyAPI.sql
    Signed-off-by: Patricio Rodriguez <159083683+PatricioRodriguez080@users.noreply.github.com>

--- 06_ReportesyAPI.sql
+++ 06_ReportesyAPI.sql
@@ -1,7 +1,7 @@
 /*
 ===============================================================================
 Materia:          Bases de Datos Aplicadas
-Comisión:         01-2900
+ComisiÃ³n:         01-2900
 Grupo:            G08
 Fecha de Entrega: 04/11/2025
 Integrantes:
@@ -9,6 +9,6 @@ Integrantes:
     - Rodriguez Arrien, Juan Manuel (44259478)
     - Rodriguez, Patricio (45683229)
     - Ruiz, Leonel Emiliano (45537914)
-Enunciado:        "05 - Creación de Reportes y APIs"
+Enunciado:        "05 - CreaciÃ³n de Reportes y APIs"
 ===============================================================================
 */
