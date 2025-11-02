36d8b1b23e7344023b941be29589651f993b2b58
Ismael Bentancur <ismaelbentancursuarez@gmail.com>
2025-11-02 20:01:14 -0300

    Medidas de seguridad

---
+++ 05_ReportesyAPI.sql
@@ -0,0 +1,14 @@
+/*
+===============================================================================
+Materia:          Bases de Datos Aplicadas
+Comisión:         01-2900
+Grupo:            G08
+Fecha de Entrega: 04/11/2025
+Integrantes:
+    - Bentancur Suarez, Ismael (45823439)
+    - Rodriguez Arrien, Juan Manuel (44259478)
+    - Rodriguez, Patricio (45683229)
+    - Ruiz, Leonel Emiliano (45537914)
+Enunciado:        "05 - Creación de Reportes y APIs"
+===============================================================================
+*/
