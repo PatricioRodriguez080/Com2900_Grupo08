8afb6684808dc25bb331e881b8b20ef7b973a4f6
Ismael Bentancur <ismaelbentancursuarez@gmail.com>
2025-11-11 19:03:01 -0300

    Formateo

--- 00_CreacionBaseDeDatos.sql
+++ 00_CreacionBaseDeDatos.sql
@@ -1,15 +1,15 @@
 /*
 ================================================================================
-Materia:         Bases de Datos Aplicadas
-Comisión:        Com 01-2900
-Grupo:           G08
-Fecha de Entrega: 25/10/2025
+Materia:          Bases de Datos Aplicadas
+Comisión:         01-2900
+Grupo:            G08
+Fecha de Entrega: 04/11/2025
 Integrantes:
-    Bentancur Suarez, Ismael 45823439
-    Rodriguez Arrien, Juan Manuel 44259478
-    Rodriguez, Patricio 45683229
-    Ruiz, Leonel Emiliano 45537914
-Enunciado:       "00 - Creación de Base de Datos"
+    - Bentancur Suarez, Ismael (45823439)
+    - Rodriguez Arrien, Juan Manuel (44259478)
+    - Rodriguez, Patricio (45683229)
+    - Ruiz, Leonel Emiliano (45537914)
+Enunciado:        "00 - Creación de Base de Datos"
 ================================================================================
 */
 
